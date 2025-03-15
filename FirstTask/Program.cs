0f92f39b712b86869c8975a38c972073dd5a06e6
Afnan Abo-Asal <afnanalaa49@gmail.com>
2025-03-15 20:54:48 +0200

    first commit

---
+++ FirstTask/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            string name = "Ramadan";
+            Console.WriteLine($"{greeting}, {name}!");
+        }
+    }
+}
